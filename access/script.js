010725008b607629f20659893a7568ae84407578
Yepssy <jessy.fallavier@lilo.org>
2020-04-05 16:26:43 +0200

    def

--- access/script.js
+++ access/script.js
@@ -1,13 +1,39 @@
+function up(scrollPos){
+    let number  = ( Math.floor(scrollPos/window.innerHeight) + 2 ) * -1 + 1 - 1;
+    let page = "#" + ancres[number];
+
+    window.location.href = page;
+
+    return false;
+}
+
+function down(scrollPos){
+    let number  = Math.floor(scrollPos/window.innerHeight) * -1 + 1 - 1;
+    if (number == 0){
+        number++;
+    }
+    let page = "#" + ancres[number];
+    
+    window.location.href = page;
+    
+    return false;
+}
+
+var venue = true
 // Initial state
 var scrollPos = 0;
+var ancres = ["profil", "enseignement", "experience", "competences", "langues", "interets"]
 // adding scroll event
 window.addEventListener('scroll', function(){
   // detects new state and compares it with the new one
-  if ((document.body.getBoundingClientRect()).top > scrollPos)
-
-		console.log(scrollPos);
+  if (venue){
+    if ((document.body.getBoundingClientRect()).top > scrollPos)
+		venue = up(scrollPos);
     else
-        console.log(scrollPos);
-	// saves the new position for iteration.
-	scrollPos = (document.body.getBoundingClientRect()).top;
+        venue = down(scrollPos);
+  }
+  else{
+      venue = true
+  }
+  scrollPos = (document.body.getBoundingClientRect()).top;
 });
