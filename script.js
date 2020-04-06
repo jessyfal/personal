f6901ab37ddd810a8c71c5c886862b2298cdcf7c
Yepssy <jessy.fallavier@lilo.org>
2020-04-06 19:03:49 +0200

    code

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
-function up(n){var o=-1*(Math.floor((n-200)/window.innerHeight)+2)+1-1;-1==o&&(o=0);var e="#"+ancres[o];window.location.href=e}function down(n){var o=-1*Math.floor(n/window.innerHeight)+1-1;0==o&&o++;var e="#"+ancres[o];window.location.href=e}var points=0,scrollPos=0,ancres=["profil","enseignement","experience","competences","langues","interets"];window.addEventListener("scroll",function(){document.body.getBoundingClientRect().top>scrollPos?20==points?(points++,up(scrollPos),points=-30):points++:20==points?(points++,down(scrollPos),points=-30):points++,scrollPos=document.body.getBoundingClientRect().top});
+function up(scrollPos) {
+    var number  = ( Math.floor((scrollPos-200 )/window.innerHeight) + 2 ) * -1 + 1 - 1;
+    if (number == -1)
+        number = 0;
+
+    var page = "#" + ancres[number];
+    window.location.href = page;
+}
+
+function down(scrollPos) {
+    var number  = Math.floor(scrollPos/window.innerHeight) * -1 + 1 - 1;
+    if (number == 0)
+        number++;
+    var page = "#" + ancres[number];
+    
+    window.location.href = page;
+
+    
+}
+
+function progressCursor(scrollPos){
+    var max = window.scrollMaxY;
+
+    var pourcentage = scrollPos * -1 / max * 100 + "vh"
+    print(pourcentage)
+    document.documentElement.style.setProperty('--h', pourcentage)
+
+}
+
+var points = 0;
+var scrollPos = 0;
+var ancres = ["profil", "enseignement", "experience", "competences", "langues", "interets"]
+
+
+window.addEventListener('scroll', function(){
+    if ((document.body.getBoundingClientRect()).top > scrollPos){
+        if (points == 20){
+            points++;
+            up(scrollPos);
+            points = -30
+        }
+        else
+            points++
+    }
+    else{
+        if (points == 20){
+            points++;
+            down(scrollPos);
+            points = -30
+        }
+        else
+            points++
+        
+    }
+    scrollPos = (document.body.getBoundingClientRect()).top;
+
+    progressCursor(scrollPos);
+
+
+});
